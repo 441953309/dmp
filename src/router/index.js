b6b5a2834379151564b93b990b37a0e4c5d718f1
Qoo <441953309@qq.com>
2016-10-13 00:41:05 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ router.get('/s/:type/:group_id', ad.getAdScript); //获取广告js代码
 router.get('/a/:group_id', ad.getAdGroup);        //获取广告内容
 router.get('/j/:type/:group_id/:ad_id', ad.jump); //跳转链接
 
+router.get('/', ctx => ctx.body='123');
+
 export default function configRoutes(app) {
   app.use(mount('/', router.routes()));
 }
