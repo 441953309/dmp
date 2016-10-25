2f092a3f47dcb4d30259e466607dc2194176d1b9
Qoo <441953309@qq.com>
2016-10-25 09:49:27 +0800

    no message

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ const config = {
     secrets: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
   },
 
-  host: 'http://d1.mobaders.com',
+  host: 'http://d2.mobaders.com',
   // db: 'mongodb://localhost/mobaders'
   db: 'mongodb://mobaders:2283250@dds-2zedcc03374ee6c41.mongodb.rds.aliyuncs.com:3717,dds-2zedcc03374ee6c42.mongodb.rds.aliyuncs.com:3717/mobaders?replicaSet=mgset-2002845'
 };
