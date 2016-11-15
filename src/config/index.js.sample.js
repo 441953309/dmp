dd1c559958541ee2e2e58b238b09dd4a1d28d29f
Qoo <441953309@qq.com>
2016-11-15 18:31:47 +0800

    no message

---
+++ src/config/index.js.sample.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const config = {
+  // host: 'http://d2.mobaders.com',
+  host: 'http://localhost:8080',
+  db: 'mongodb://localhost/mobaders'
+  // db: 'mongodb://localhost/mobaders-android'
+};
+
+module.exports = config;
